9cbbff2f61f7139c9bf25bd0b2145fe48509ef8d
Kris <kris@syevens.com>
2022-09-28 23:57:26 -0500

    Update Commit - corrected state mgmt dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">Shopping Cart</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
